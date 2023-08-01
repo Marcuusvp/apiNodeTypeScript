3abc5aeef00e97433c5a8f37bd169e7981409426
Marcuusvp <marcusviniciuspereira1@gmail.com>
2023-08-01 17:52:49 -0300

    Adicao e configuracao eslint

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const server = express();
 
 server.get('/', (req, res)=> {
-    return res.send('Olá Dev!');
+  return res.send('Olá Dev!');
 });
 
 export { server };
